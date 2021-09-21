7a580e460a7aa5d6a8e8dbbb6bd7bdb34e7b74d9
Joao-Quinta <qjoao.filipe@gmail.com>
2021-09-21 14:05:02 +0200

    j

--- metaheuristique_pour_optimisation/TP_0/TP_0.py
+++ metaheuristique_pour_optimisation/TP_0/TP_0.py
@@ -1,15 +1,15 @@
 import numpy as np
+import matplotlib as plt
 import math
 import random
 
 
 # N -> number of faces, n -> number of rolls
 def balancedDice(N, n):
-    arr = [random.randint(1, N) for _ in range(n)]
-    arr2 = [math.floor(random.random() * N) for _ in range(n)]
-
-    print(arr)
-    print(arr2)
+    arr = [math.floor(random.random() * N) for _ in range(n)]
 
 
-balancedDice(6, 10)
+    for i in range(0, N):
+        print("val -> ", i, " : ", arr.count(i))
+
+balancedDice(6, 10000)
