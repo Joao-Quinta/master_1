6af8ac121fd42421616cc48b43f693ded5f5fbd2
Joao-Quinta <qjoao.filipe@gmail.com>
2021-09-24 14:17:23 +0200

    exos_meta_done

--- metaheuristique_pour_optimisation/TP_0/TP_0.py
+++ metaheuristique_pour_optimisation/TP_0/TP_0.py
@@ -1,111 +1,91 @@
+# Joao Quinta
+# metaheuristique pour l optimisation
+
 import numpy as np
-import matplotlib as plt
-import copy
-import math
-import random
+import functions
 
 
 # N -> number of faces, n -> number of rolls
-def balancedDice(N, n):
+def exo_1(N, n):
     print()
     print("balanced dice: ", n, " rolls, ", N, " sides.")
     print()
-    arr = [math.floor(random.random() * N) for _ in range(n)]
+    arr = [functions.balancedDice(N) for _ in range(n)]
 
     for i in range(0, N):
         print("val -> ", i, " : ", arr.count(i))
 
 
-balancedDice(6, 1000)
+exo_1(6, 1000)
 
 
 # P -> array of probabilities, n -> number of rolls
-def biasedDice_balancedDice(P, n):
+def exo_2(P, n):
     print()
     print("biased dice: ", n, " rolls, ", P, " -> Pi")
     print()
-    min = np.min(P)
 
-    realEvent = []
-    for i in range(len(P)):
-        for j in range(int(P[i] / min)):
-            realEvent.append(i)
+    transformed_dice = functions.transform_biased_balanced(P)
+    number_of_faces = len(transformed_dice)
 
-    arr = [realEvent[math.floor(random.random() * int(1 / min))] for _ in range(n)]
+    arr = [transformed_dice[functions.balancedDice(number_of_faces)] for _ in range(n)]
 
     for i in range(0, len(P)):
         print("val -> ", i, " : ", arr.count(i))
 
 
-biasedDice_balancedDice([1 / 4, 1 / 4, 1 / 4, 1 / 8, 1 / 16, 1 / 16], 1000)
+exo_2([1 / 4, 1 / 4, 1 / 4, 1 / 8, 1 / 16, 1 / 16], 1000)
 
 
 # p = probability of tail, n = number o tosses, type -> fast use with other function
-def coinToss(p, n, type):
-    if type == 0:
-        print()
-        print("coin toss: ", n, " tosses, ", p, " probability of tail")
-        print()
+def exo_3(p, n):
+    print()
+    print("coin toss: ", n, " tosses, ", p, " probability of tail")
+    print()
 
     # gamma is either 1 or 0 -> arr is filled with 0 and 1
     # 1 has probability of p, and 0 probability of 1 - p
     # lets say p = 1, math.floor(random.random() + p) = 1, for all random.random()
     # 1 is tail, 0 is head
-    arr = [math.floor(random.random() + p) for _ in range(n)]
+    arr = [functions.coinToss(p) for _ in range(n)]
 
-    if type == 1:
-        # print(p, type, arr)
-        return arr[0]
-    else:
-        print("tail -> ", arr.count(1))
-        print("head -> ", arr.count(0))
+    print("tail -> ", arr.count(1))
+    print("head -> ", arr.count(0))
 
-        arr = np.array(arr)
-        arr = arr * p + (1 - arr) * (1 - p)
+    arr = np.array(arr)
+    arr = arr * p + (1 - arr) * (1 - p)
 
 
-coinToss(0.7, 1000, 0)
+exo_3(0.7, 1000)
 
 
 # n -> number of rolls, P -> probabilities
-def biasedDice_biasedCoin(P, n):
+def exo_4(P, n):
     print()
-    print("dice roll: ", n, " rolls, ", P, " -> Pi")
+    print("dice roll (biased coin): ", n, " rolls, ", P, " -> Pi")
     print()
 
-    P_temp = []
-    arr = []
-
-    roll = 0
-    while roll < n:
-        if len(P_temp) == 0:
-            P_temp = copy.copy(P)
-        sumP = np.sum(P_temp)
-        if sumP != 1.0:
-            P_temp = [P_temp[i] / sumP for i in range(len(P_temp))]
-
-        first = P_temp.pop(0)
-        if coinToss(first, 1, 1) == 1:
-            arr.append(len(P) - (len(P_temp) + 1))
-            roll += 1
+    arr = [functions.biasedDice_biasedCoin(P) for _ in range(n)]
 
     for i in range(0, len(P)):
         print("val -> ", i, " : ", arr.count(i))
 
 
-biasedDice_biasedCoin([1 / 4, 1 / 4, 1 / 4, 1 / 8, 1 / 16, 1 / 16], 10000)
+exo_4([1 / 4, 1 / 4, 1 / 4, 1 / 8, 1 / 16, 1 / 16], 1000)
 
 
 # n -> number of rolls, P -> probabilities
-def rouletteMethod(P, n):
-    P_comul = []
-    for j in range(len(P)):
-        for i in range(j):
-            P_comul.append(np.sum(P[0:i + 1]))
-    print(P_comul)
-    # np.sum(P[0:1]) -> only 1 element
-    # print(np.sum(P[0:1]))
-    # print()
+def exo_5(P, n):
+    print()
+    print("roulette roll: ", n, " rolls, ", P, " -> Pi")
+    print()
+
+    P_comul = functions.compute_P_comul(P)
+
+    arr = [functions.rouletteMethod(P_comul) for _ in range(n)]
+
+    for i in range(0, len(P)):
+        print("val -> ", i, " : ", arr.count(i))
 
 
-rouletteMethod([1 / 4, 1 / 4, 1 / 4, 1 / 8, 1 / 16, 1 / 16], 10000)
+exo_5([1 / 4, 1 / 4, 1 / 4, 1 / 8, 1 / 16, 1 / 16], 1000)
