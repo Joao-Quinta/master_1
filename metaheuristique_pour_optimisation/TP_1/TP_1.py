e8da33f800d84dc8ee1f563323c8dab629ac8c36
Joao-Quinta <qjoao.filipe@gmail.com>
2021-10-01 19:39:20 +0200

    deterministic_done

--- metaheuristique_pour_optimisation/TP_1/TP_1.py
+++ metaheuristique_pour_optimisation/TP_1/TP_1.py
@@ -6,6 +6,44 @@ import random
 import numpy as np
 
 
+def compute_fitness(X, k):
+    fitness = 0
+    if k == 0:
+        for i in range(len(X) - k):
+            str_comp = X[i:i + 1 + k]
+            if str_comp == "0":
+                val = 2
+            else:
+                val = 1
+            fitness = fitness + val
+
+    elif k == 1:
+        for i in range(len(X) - k):
+            str_comp = X[i:i + 1 + k]
+            if str_comp == "00":
+                val = 2
+            elif str_comp == "01":
+                val = 3
+            elif str_comp == "10":
+                val = 2
+            else:
+                val = 0
+            fitness = fitness + val
+
+    else:
+        for i in range(len(X) - k):
+            str_comp = X[i:i + 1 + k]
+            if str_comp == "001" or str_comp == "010":
+                val = 1
+            elif str_comp == "100":
+                val = 2
+            else:
+                val = 0
+            fitness = fitness + val
+
+    return fitness
+
+
 def coinToss(p):
     # same computation as shown in the TP question
     return math.floor(random.random() + p)
@@ -29,9 +67,40 @@ def generate_neighbors(X):
     return list_neighbors
 
 
+def find_best_in_neighborhood(array_neighbors, k):
+    max_fitness_value = -1
+    best_x = ""
+    for neighbor in array_neighbors:
+        neighbor_fit = compute_fitness(neighbor, k)
+        if neighbor_fit > max_fitness_value:
+            max_fitness_value = neighbor_fit
+            best_x = neighbor
+
+    return best_x, max_fitness_value
+
+
+def deterministic_hill_climb(N, k):
+    X = generate_X(N)
+    X_fitness = compute_fitness(X, k)
+    X_neighbors = generate_neighbors(X)
+    com = 0
+    while True:
+        X_neighbors_fitness_str, X_neighbors_fitness_val, = find_best_in_neighborhood(X_neighbors, k)
+        if X_neighbors_fitness_val > X_fitness:
+            X = X_neighbors_fitness_str
+            X_fitness = X_neighbors_fitness_val
+            X_neighbors = generate_neighbors(X)
+            com = com + 1
+        else:
+            print(com)
+            break
+    return X
+
+
+"""
 X = generate_X(5)
 print(X)
 X_neighbors = generate_neighbors(X)
 print(X_neighbors)
-
-# 1110 - 0010 - 0100 - 0111
+"""
+print(deterministic_hill_climb(50, 2))
