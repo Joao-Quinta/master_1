1a0fbaa50342ca15fbe5fa0717bb521ff151d816
Joao-Quinta <qjoao.filipe@gmail.com>
2021-12-13 14:22:53 +0100

    some results

--- metaheuristique_pour_optimisation/TP_6/TP6_CostadaQuinta_JoaoFilipe.py
+++ metaheuristique_pour_optimisation/TP_6/TP6_CostadaQuinta_JoaoFilipe.py
@@ -20,6 +20,8 @@ def rouletteMethod(P_comul):
             return i
 
 
+# randomly generates population, an individual is a concatenation of x and y in binary form, x and y are 10 bits each
+# x and y are number between 10 and 1000
 def generate_population(N):
     population = []
     for _ in range(N):
@@ -33,11 +35,12 @@ def generate_population(N):
     return population
 
 
+# maps binary value to real number between 10 and 1000
 def map_value(v):
     return ((v / np.power(2, 10)) * (1000 - 10)) + 10
 
 
-# TODO -> compute fitness function, receives single individual
+# computes fitness of an individual
 def compute_fitness(individual):
     x = map_value(int(individual[:10], 2))
     y = map_value(int(individual[10:], 2))
@@ -50,22 +53,17 @@ def selection_random(population):
     fit_total = np.sum(fit_population)
     random_weights = [fit_population[i] / fit_total for i in range(len(fit_population))]
     return [fit_population[rouletteMethod(random_weights)] for _ in
-            range(len(population))]  # TODO check if this works well
+            range(len(population))]
 
 
 # selection step, based on tournament 5 strategy
-def selection_tournament_5(population):  # TODO check if this works well
+def selection_tournament_5(population):
     new_gen_pop = []
     fit_population = [compute_fitness(population[i]) for i in range(len(population))]
-    print(fit_population)
     for l in range(len(population)):
-        print()
         index_of_selection = [balancedDice(len(population)) for _ in range(5)]
-        print("index of selection : ", index_of_selection)
         selection_of_5 = [fit_population[i] for i in index_of_selection]
-        print("selection of 5 : ", selection_of_5)
         new_gen_pop.append(population[index_of_selection[np.argmax(selection_of_5)]])
-        print(new_gen_pop)
     return new_gen_pop
 
 
@@ -96,3 +94,57 @@ def mutation(population, p):
                     individual[j] = '0'
         population[i] = ''.join(individual)
     return population
+
+
+# global function
+def genetic_algorithm(N, p, max_generation):
+    generation = 0
+    population = generate_population(N)
+    while generation < max_generation:
+        generation = generation + 1
+        population = selection_tournament_5(population)
+        # population = crossing(population)
+        population = mutation(population, p)
+    return population[np.argmax([compute_fitness(population[i]) for i in range(len(population))])]
+
+
+x = [10, 100, 1000, 10000, 100000]
+for x_ in x:
+    r = genetic_algorithm(100, 0.01, x_)
+    print(r, " - > ", compute_fitness(r))
+
+"""
+WITH CROSSING : 
+
+p = 0.1
+
+00100110100000000000  - >  -11.966731164990291
+00000000010000000110  - >  -14.119453180124616
+00000000000000000000  - >  -8.518127505726877
+00000111100000000001  - >  -10.213980556320166
+00000000000000000001  - >  -9.313415632112324
+
+p = 0.01
+00000111100000000000  - >  -9.394785809777554
+00000111100000000000  - >  -9.394785809777554
+00111101010000000000  - >  -9.460929749396577
+00100110010000000000  - >  -8.915082864815007
+01111010100000000000  - >  -9.737136305702137
+
+WITHOUT CROSSING : 
+
+p = 0.1
+00010100010000000100  - >  -13.331906952554519
+00000000000000000000  - >  -8.518127505726877
+00000000000000000001  - >  -9.313415632112324
+00000000000000000000  - >  -8.518127505726877
+00000000000000000000  - >  -8.518127505726877
+
+p = 0.01
+00111101010000000000  - >  -9.460929749396577
+10100000110000000000  - >  -9.147215243893433
+00000000000000000000  - >  -8.518127505726877
+00000000000000000000  - >  -8.518127505726877
+00111101010000000000  - >  -9.460929749396577
+
+"""
